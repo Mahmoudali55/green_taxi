a998630d79b42e1795d660aa8d5707b7d094e269
mahmoudali15 <130462575+mahmoudali15@users.noreply.github.com>
2023-09-21 14:48:38 +0300

    appcolor_and_appcontants

---
+++ lib/utils/app_constants.dart
@@ -0,0 +1,13 @@
+class AppConstants {
+  static const String helloNiceToMeetYou = 'Hello,nice to meet you!';
+  static const String getMovingWithGreenTaxi = 'Get Moving With Green Taxi';
+  static const String enterMobileNumber = 'Enter your Mobile Number';
+  static const String byCreating = 'By creating an account , you agree to our';
+  static const String trensOfSrevice = 'Terms of Service';
+  static const String of = 'of';
+  static const String privacyPolicy = ' privacy Policy';
+  static const String phoneVerification = 'phone Verification';
+  static const String enterOtp = 'Enter your OTP code below';
+  static const String resendCode = 'Resend code in';
+  static const String seconds = 'seconds';
+}
