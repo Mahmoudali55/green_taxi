5f4e1081ca489f09c448b0538124c2ec5439f5ca
mahmoudali15 <130462575+mahmoudali15@users.noreply.github.com>
2023-09-22 19:09:48 +0300

    create_homescreen_and_googlemap

--- lib/views/home_screen.dart
+++ lib/views/home_screen.dart
@@ -1,13 +1,137 @@
-import 'package:flutter/material.dart';
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
-class HomeScreen extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:flutter/services.dart' show rootBundle;
+
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  String? _mapStyle;
+  @override
+  void initState() {
+    super.initState();
+    rootBundle.loadString('assets/map_style.txt').then((string) {
+      _mapStyle = string;
+    });
+  }
+
+  final CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
+  GoogleMapController? mymapController;
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Logedin'),
+      body: Stack(children: [
+        Positioned(
+          top: 150,
+          left: 0,
+          right: 0,
+          bottom: 0,
+          child: GoogleMap(
+            onMapCreated: (GoogleMapController controller) {
+              mymapController = controller;
+              mymapController!.setMapStyle(_mapStyle);
+            },
+            initialCameraPosition: _kGooglePlex,
+          ),
+        ),
+        bulidProfileTile(),
+        bulidProfileField()
+      ]),
+    );
+  }
+
+  Widget bulidProfileTile() {
+    return Positioned(
+        top: 60,
+        left: 20,
+        right: 20,
+        child: Container(
+          width: Get.width,
+          child: Row(
+            children: [
+              Container(
+                width: 60,
+                height: 60,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                      image: AssetImage('assets/prson.png'), fit: BoxFit.fill),
+                ),
+              ),
+              SizedBox(
+                width: 15,
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  RichText(
+                      text: TextSpan(children: [
+                    TextSpan(
+                        text: 'Good Morning',
+                        style: TextStyle(color: Colors.black, fontSize: 14)),
+                    TextSpan(
+                        text: 'Mark',
+                        style: TextStyle(
+                            color: Colors.green,
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold))
+                  ])),
+                  Text(
+                    'Where are  you going?',
+                    style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black),
+                  )
+                ],
+              )
+            ],
+          ),
+        ));
+  }
+
+  Widget bulidProfileField() {
+    return Positioned(
+      top: 170,
+      left: 20,
+      right: 20,
+      child: Container(
+        width: Get.width,
+        height: 50,
+        decoration: BoxDecoration(
+            color: Colors.white,
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.05),
+                spreadRadius: 7,
+                blurRadius: 10,
+              ),
+            ],
+            borderRadius: BorderRadius.circular(8)),
+        child: TextFormField(
+          style: GoogleFonts.poppins(
+              fontSize: 14,
+              fontWeight: FontWeight.w600,
+              color: Color(0xffA7A7A7)),
+          decoration: InputDecoration(
+            suffixIcon: Padding(
+              padding: const EdgeInsets.only(left: 10),
+              child: Icon(Icons.search),
+            ),
+            border: InputBorder.none,
+          ),
+        ),
       ),
     );
   }
